288a155421bceffa84c42cc324569564d80dc610
sarath3426 <sarathlogin2creations@gmail.com>
2025-01-28 17:24:42 +0530

    work details add popup

--- lib/features/work_details/views/work_details_screen.dart
+++ lib/features/work_details/views/work_details_screen.dart
@@ -7,10 +7,12 @@ import 'package:site_720/core/widgets/appbar.dart';
 import 'package:site_720/core/widgets/shimmer.dart';
 import 'package:site_720/features/work_details/cubit/work_details_state.dart';
 
+import '../../../core/widgets/buttons.dart';
+import '../../../core/widgets/snack_bar.dart';
 import '../cubit/work_details_cubit.dart';
 
 class WorkDetails extends StatelessWidget {
-  const WorkDetails({super.key});
+   const WorkDetails({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +22,70 @@ class WorkDetails extends StatelessWidget {
         builder: (context, state) {
           return Scaffold(
               backgroundColor: AppColors.backgroundColor,
-              appBar: simpleAppbar(context, "Work Details"),
+              appBar:PreferredSize(
+          preferredSize:
+              Size.fromHeight(MediaQuery.of(context).size.height * 0.2),
+          child: Container(
+            height: MediaQuery.of(context).size.height * .15,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/appbar.png"),
+                    fit: BoxFit.fill),
+                color: AppColors.primaryColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25),
+                )),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 35, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(
+                          Icons.arrow_back_ios,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Text(
+                        "Work Details",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                            fontFamily: "Lobster",
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  InkWell(
+                    onTap: () {
+                     workDialog(context);
+                    },
+                    child: const CircleAvatar(
+                      radius: 20,
+                      backgroundColor: AppColors.lightPrimary,
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
               body: ListView.builder(
                 shrinkWrap: true,
                 padding: const EdgeInsets.all(8),
@@ -173,4 +238,196 @@ class WorkDetails extends StatelessWidget {
       ),
     );
   }
+}
+
+ Future<void> workDialog(BuildContext context) async {
+  String isWorking = 'Yes'; // Default value for radio button
+  String? selectedStatus; // For the status dropdown
+  final List<String> statuses = ["Company Issue", "Client Issue", "Payment Pending", "General Issue"];
+
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        backgroundColor: Colors.white,
+        content: SizedBox(
+          height: 500,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 16.0, bottom: 25),
+                  child: Text(
+                    "Add Work Details",
+                    style: TextStyle(
+                      color: AppColors.primaryColor,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const Text(
+                  "Are You Sure Today is Working?",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold,color:AppColors.errorColor),
+                ),
+                Row(
+                  children: [
+                    Radio<String>(
+                      value: 'Yes',
+                      groupValue: isWorking,
+                      onChanged: (value) {
+                        isWorking = value!;
+                        (context as Element).markNeedsBuild();
+                      },
+                    ),
+                    const Text("Yes"),
+                    Radio<String>(
+                      value: 'No',
+                      groupValue: isWorking,
+                      onChanged: (value) {
+                        isWorking = value!;
+                        (context as Element).markNeedsBuild();
+                      },
+                    ),
+                    const Text("No"),
+                  ],
+                ),
+                const SizedBox(height: 12),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.95,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == "") {
+                        return "Enter Date";
+                      }
+                      return null;
+                    },
+                    onTap: () {
+                     
+                    },
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.all(10),
+                      labelText: 'Date',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      prefixIcon: const Icon(Icons.calendar_today),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 12),
+                if (isWorking == "Yes") ...[
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value == "") {
+                          return "Enter Number of Labours";
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.all(10),
+                        labelText: 'No of Labours*',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        prefixIcon: const Icon(Icons.group),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                ] else ...[
+                 Container(
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: DropdownButtonFormField<String>(
+                      value: selectedStatus,
+                      items: statuses.map((status) {
+                        return DropdownMenuItem(
+                          value: status,
+                          child: Text(status),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        selectedStatus = value;
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Select a Status";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.all(10),
+                        labelText: 'Status*',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        prefixIcon: const Icon(Icons.info),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                ],
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.95,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == "") {
+                        return "Enter Description";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.text,
+                    maxLines: 3,
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.all(10),
+                      labelText: 'Description',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      prefixIcon: const Icon(Icons.text_fields),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () async {
+                    snackBar(context, 'Work Details Added Successfully!', Colors.black);
+                    Navigator.pop(context);
+                  },
+                  child: LargeButton(title: "Add"),
+                ),
+                  Center(
+                    child: TextButton(
+                      onPressed: () {
+                        // Clear or reset fields if needed
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Close'),
+                    ),
+                  ),
+
+              ],
+            ),
+          ),
+        ),
+      );
+    },
+  );
 }

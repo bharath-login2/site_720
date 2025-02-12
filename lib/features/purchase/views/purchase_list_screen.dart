d7ac4f9fe61d9e52676f9a2dfb3f62756b36653e
ansarek8630 <jithintbr@gmail.com>
2025-02-12 09:22:38 +0530

    change

--- lib/features/purchase/views/purchase_list_screen.dart
+++ lib/features/purchase/views/purchase_list_screen.dart
@@ -91,158 +91,181 @@ class PurchaseList extends StatelessWidget {
           ),
           body: BlocBuilder<PurchaseCubit, PurchaseState>(
             builder: (context, state) {
-              return state is PurchaseSuccess? ListView.builder(
-                shrinkWrap: true,
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                itemCount: state.response.data.length,
-                
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(6.0),
-                    child: InkWell(
-                      onTap: () {
-                        // Navigator.of(context).pushNamed(AppRoutes.stageHistory);
-                      },
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * .9,
-                        height: MediaQuery.of(context).size.height * .14,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.8),
-                              blurRadius: 3,
-                              offset: const Offset(0, 3),
-                            ),
-                          ],
-                        ),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width * .7,
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                                top: 8.0, left: 8.0, right: 8.0, bottom: 8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Column(
+              return state is PurchaseSuccess
+                  ? ListView.builder(
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      itemCount: state.response.data.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: InkWell(
+                              onTap: () {
+                                // Navigator.of(context).pushNamed(AppRoutes.stageHistory);
+                              },
+                              child: Container(
+                                width: MediaQuery.of(context).size.width * .9,
+                                height:
+                                    MediaQuery.of(context).size.height * .14,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.8),
+                                      blurRadius: 3,
+                                      offset: const Offset(0, 3),
+                                    ),
+                                  ],
+                                ),
+                                child: SizedBox(
+                                  width: MediaQuery.of(context).size.width * .7,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 8.0,
+                                        left: 8.0,
+                                        right: 8.0,
+                                        bottom: 8.0),
+                                    child: Column(
                                       crossAxisAlignment:
                                           CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
                                       children: [
-                                        Text(
-                                         state.response.data[index].materialName,
-                                          style: const TextStyle(
-                                              fontSize: 14,
-                                              fontWeight: FontWeight.bold,
-                                              color: AppColors.coffie),
+                                        Row(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  state.response.data[index]
+                                                      .materialName,
+                                                  style: const TextStyle(
+                                                      fontSize: 14,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      color: AppColors.coffie),
+                                                ),
+                                                //  Text(
+                                                //    "Bill Date: ${state.response.data[index].billDate}",
+                                                //   style: const TextStyle(
+                                                //     fontSize: 12,
+                                                //     fontWeight: FontWeight.bold,
+                                                //   ),
+                                                // ),
+                                              ],
+                                            ),
+                                            Text(
+                                              DateFormat('yyyy-MM-dd').format(
+                                                  state.response.data[index]
+                                                      .billDate),
+                                              style: const TextStyle(
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ],
                                         ),
-                                        //  Text(
-                                        //    "Bill Date: ${state.response.data[index].billDate}",
-                                        //   style: const TextStyle(
-                                        //     fontSize: 12,
-                                        //     fontWeight: FontWeight.bold,
-                                        //   ),
-                                        // ),
+                                        Row(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.end,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Row(
+                                              children: [
+                                                AmountContainer(
+                                                    title: "Quantity",
+                                                    amount: state.response
+                                                        .data[index].quantity),
+                                                const SizedBox(
+                                                  width: 10,
+                                                ),
+                                                AmountContainer(
+                                                    title: "Amount",
+                                                    amount:
+                                                        "₹ ${state.response.data[index].grandTotal}"),
+                                              ],
+                                            ),
+                                            Row(
+                                              children: [
+                                                Container(
+                                                  height: 25,
+                                                  width: 25,
+                                                  decoration: BoxDecoration(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            5),
+                                                    color: AppColors.lightBlue,
+                                                    boxShadow: [
+                                                      BoxShadow(
+                                                        color: Colors.grey
+                                                            .withOpacity(0.8),
+                                                        blurRadius: 6,
+                                                        offset:
+                                                            const Offset(1, 1),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                  child: const Icon(
+                                                    Icons.edit,
+                                                    size: 18,
+                                                    color: Colors.white,
+                                                  ),
+                                                ),
+                                                const SizedBox(
+                                                  width: 7,
+                                                ),
+                                                Container(
+                                                  height: 25,
+                                                  width: 25,
+                                                  decoration: BoxDecoration(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            5),
+                                                    color: Colors.red,
+                                                    boxShadow: [
+                                                      BoxShadow(
+                                                        color: Colors.grey
+                                                            .withOpacity(0.8),
+                                                        blurRadius: 6,
+                                                        offset:
+                                                            const Offset(1, 1),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                  child: const Icon(
+                                                    Icons.delete,
+                                                    size: 18,
+                                                    color: Colors.white,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
                                       ],
                                     ),
-                                    Text(
-                                  DateFormat('yyyy-MM-dd').format(state.response.data[index].billDate),
-                                  style: const TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
                                   ),
                                 ),
-                                  ],
-                                ),
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        AmountContainer(
-                                            title: "Quantity", amount: state.response.data[index].quantity),
-                                        const SizedBox(
-                                          width: 10,
-                                        ),
-                                        AmountContainer(
-                                            title: "Amount", amount: "₹ ${state.response.data[index].grandTotal}"),
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [
-                                        Container(
-                                          height: 25,
-                                          width: 25,
-                                          decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(5),
-                                            color: AppColors.lightBlue,
-                                            boxShadow: [
-                                              BoxShadow(
-                                                color: Colors.grey
-                                                    .withOpacity(0.8),
-                                                blurRadius: 6,
-                                                offset: const Offset(1, 1),
-                                              ),
-                                            ],
-                                          ),
-                                          child: const Icon(
-                                            Icons.edit,
-                                            size: 18,
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                        const SizedBox(
-                                          width: 7,
-                                        ),
-                                        Container(
-                                          height: 25,
-                                          width: 25,
-                                          decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(5),
-                                            color: Colors.red,
-                                            boxShadow: [
-                                              BoxShadow(
-                                                color: Colors.grey
-                                                    .withOpacity(0.8),
-                                                blurRadius: 6,
-                                                offset: const Offset(1, 1),
-                                              ),
-                                            ],
-                                          ),
-                                          child: const Icon(
-                                            Icons.delete,
-                                            size: 18,
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
+                              ),
+                            ));
+                      },
                     )
-                  );
-                },
-              ):
-              state is PurchaseLoading?ListView.builder(itemBuilder: (context, index) {
-               shimmerContainer(100, 70);
-              },): const Center(
-                child: Text("No Bills"),
-              );
+                  : state is PurchaseLoading
+                      ? ListView.builder(
+                          itemBuilder: (context, index) {
+                            return shimmerContainer(100, 70);
+                          },
+                        )
+                      : const Center(
+                          child: Text("No Bills"),
+                        );
             },
           )),
     );
